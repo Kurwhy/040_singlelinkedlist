3402a9607cf44ed9cfd8bb6143298405cb3ff591
Wahyu <firmansyahw503@gmail.com>
2023-05-18 17:14:59 +0700

    case 1

--- SingleLinkedList.cpp
+++ SingleLinkedList.cpp
@@ -143,7 +143,12 @@ int main() {
 			cin >> pilihan;
 			switch (pilihan)
 			{
-
+			case 1:
+				addNode();
+				cout << "Data Berhasil Ditambahkan" << endl;
+				system("pause");
+				system("cls");
+				break;
 			}
 		}
 	}
